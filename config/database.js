f483fbbe5494c5d1ccbc16c47901a85bf3309ff6
Awosise Oluwaseun <awosiseo@gmail.com>
2022-12-16 20:59:39 +0100

    new: database setup

---
+++ config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+mongoose.set("strictQuery", false)
+dotenv.config()
+
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log(
+      `Connected to Mongo! Database name: ${conn.connections[0].name}`
+    )
+  } catch (err) {
+    console.error("Error connecting to mongo", err)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
