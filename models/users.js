b9b332f5286543891dd0256118c937fd5087d72a
Awosise Oluwaseun <awosiseo@gmail.com>
2022-12-17 21:02:18 +0100

    new: user model setup

---
+++ models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please input name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please input email"]
+    },
+    phone: {
+        type: String,
+        required: [true, "Please input phone number"]
+    },
+    image: {
+        type: String,
+        required: [true, "Please insert image"]
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("User", UserSchema)
