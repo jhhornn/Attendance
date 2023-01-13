03cfc986ae02fe85491713fd9d23821a112e9e92
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-14 00:30:10 +0100

    feat: hash password before saving to DB

--- models/auth.js
+++ models/auth.js
@@ -4,35 +4,35 @@ const { Schema } = mongoose
 const { isEmail } = require("validator")
 
 const AuthSchema = new Schema({
-    email: {
-        type: String,
-        required: [true, "Please enter an email"],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, "please enter a valid email"]
-    },
-    firstName: {
-        type: String,
-        required: [true, "Please enter your first name"],
-        capitalize: true,
-    },
-    lastName: {
-        type: String,
-        required: [true, "Please enter your last name"],
-        capitalize: true,
-    },
-    password: {
-        type: String,
-        required:  [true, "Please enter password"],
-        minlength: [6, "Minimum password length is 6 characters"]
-    }
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "please enter a valid email"]
+  },
+  firstName: {
+    type: String,
+    required: [true, "Please enter your first name"],
+    capitalize: true
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please enter your last name"],
+    capitalize: true
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter password"],
+    minlength: [6, "Minimum password length is 6 characters"]
+  }
 })
 
-AuthSchema.pre("save", async function (next){
-    const salt = await bcrypt.genSalt()
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
+AuthSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
-const Auth = mongoose.model("auth",  AuthSchema)
+const Auth = mongoose.model("auth", AuthSchema)
 module.exports = Auth
