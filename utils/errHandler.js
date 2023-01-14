5edaaefa262800cde443eb76f13dbbfe9d46f35b
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-15 00:42:15 +0100

    fix: message property replaced with errors

--- utils/errHandler.js
+++ utils/errHandler.js
@@ -1,20 +1,20 @@
 const handleErrors = (err) => {
-    let errors = { email: "", password: "" }
+  let errors = { email: "", firstName: "", lastName: "", password: "" }
 
-    // duplicate error
-    if (err.code === 11000){
-        errors.email = "the email is already in use"
-        return errors
-    }
-
-    //validation errors
-    if (err.message.includes("user validation failed")){
-        // destructuring inside the forEach, similar to (err) => {err.properties}
-        Object.values(err.message).forEach(({properties}) => {
-            errors[properties.path] = properties.message
-        })
-    }
+  // duplicate error
+  if (err.code === 11000) {
+    errors.email = "the email is already in use"
     return errors
+  }
+
+  //validation errors
+  if (err.message.includes("auth validation failed")) {
+    // destructuring inside the forEach, similar to (err) => {err.properties}
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message
+    })
+  }
+  return errors
 }
 
 module.exports = handleErrors
