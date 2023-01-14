46d58600c26889496603e6b1f02701d8a235642d
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-15 00:38:05 +0100

    PRETTIFIED

--- middlewares/imageUploader.js
+++ middlewares/imageUploader.js
@@ -2,17 +2,16 @@ const multer = require("multer")
 
 // Image upload
 let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads")
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
-    }
+  destination: function (req, file, cb) {
+    cb(null, "./uploads")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
+  }
 })
 
 let upload = multer({
-    storage: storage
+  storage: storage
 }).single("image")
-
 
 module.exports = upload
