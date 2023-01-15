407f4c2f4b0480293e5b657b3c5d0aaae2888993
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-16 00:44:24 +0100

    chore: removed unwanted module

--- routes/routes.js
+++ routes/routes.js
@@ -1,135 +1,27 @@
 const express = require("express")
 const router = express.Router()
-const User = require("../models/users")
-const multer = require("multer")
-const fs = require("fs")
-
-// Image upload
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads")
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
-    }
-})
-
-let upload = multer({
-    storage: storage
-}).single("image")
-
+const imageUploader = require("../middlewares/imageUploader")
+const {
+  postDetails,
+  getHomepage,
+  getEditPage,
+  updateDetails,
+  deleteDetails
+} = require("../controllers/attendance")
 
 router.get("/add", (req, res) => {
-    res.render("pages/add_users", { title: "Add Users" })
+  res.render("pages/add_users", { title: "Add Users" })
 })
 
-router.post("/add", upload, (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        image: req.file.filename
-    })
-    user.save((err) => {
-        if(err){
-            res.json({message: err.message, type: "danger"})
-        } else {
-            req.session.message = {
-                type: "success",
-                message: "User added successfully"
-            }
-            res.redirect("/")
-        }
-    })
-})
+router.post("/add", imageUploader, postDetails)
 
-router.get("/", (req, res) => {
-    User.find().exec((err, users) => {
-        if(err) {
-            res.json({message: err.message})
-        } else {
-            res.render("pages/index", { 
-                title: "Home Page",
-                users: users
-            })
-        }
-    })
-
-})
+router.get("/", getHomepage)
 
 // Edit an user route
-router.get("/edit/:id", (req, res) => {
-    let id = req.params.id
-    User.findById(id, (err, user) => {
-        if(err) {
-            res.redirect("/")
-        }else {
-            if (user == null) {
-                res.redirect("/")
-            } else {
-                res.render("edit_users", {
-                    title: "Edit User",
-                    user: user
-                })
-            }
-        }
-    })
-})
+router.get("/edit/:id", getEditPage)
 
-router.post("/update/:id", upload, (req, res) => {
-    let id = req.params.id
-    let new_image = ""
-    if(req.file){
-        new_image = req.file.filename
-        try{
-            fs.unlinkSync("./uploads/" + req.body.old_image)
-        }catch(err){
-            console.log(err)
-        }
-    }else {
-        new_image = req.body.old_image
-    }
+router.post("/update/:id", imageUploader, updateDetails)
 
-    User.findByIdAndUpdate(id, {
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        image: new_image
-    }, (err, result) => {
-        if(err) {
-            res.json({ message: err.message, type: "danger"})
-        } else {
-            req.session.message = {
-                type: "success",
-                message: "User updated successfully"
-            }
-            res.redirect("/")
-        }
-    })
-})
-
-router.get("/delete/:id", (req,res) => {
-    let id = req.params.id
-    User.findByIdAndRemove(id, (err, result) => {
-        if(result.image != ""){
-            try{
-                fs.unlinkSync("./uploads/"+result.image)
-            }catch{
-                console.log(err)
-            }
-        }
-        if(err){
-            res.json({ message: err.message })
-        } else {
-            req.session.message = {
-                type: "info",
-                message: "User deleted successfully"
-            }
-            res.redirect("/")
-        }
-    })
-})
-
-
+router.get("/delete/:id", deleteDetails)
 
 module.exports = router
