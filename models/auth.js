138dacbb7328b1d5ab557f17883be540f8af7390
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-20 01:27:37 +0100

    feat: auth schema method to find user and compare password

--- models/auth.js
+++ models/auth.js
@@ -39,8 +39,13 @@ AuthSchema.pre("save", async function (next) {
 AuthSchema.statics.login = async function (email, password) {
   const user = await this.findOne({ email })
   if (user) {
-    bcrypt.compare(password, user.password)
+    const auth = await bcrypt.compare(password, user.password)
+    if (auth) {
+      return user
+    }
+    throw Error("Incorrect password")
   }
+  
   throw Error("Incorrect email")
 }
 
