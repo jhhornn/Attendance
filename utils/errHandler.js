b287d978941f426ab27d3ada64a585b543af114f
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-24 01:47:25 +0100

    chore: error handler to handle login email and password errors

--- utils/errHandler.js
+++ utils/errHandler.js
@@ -1,6 +1,16 @@
 const handleErrors = (err) => {
   let errors = { email: "", firstName: "", lastName: "", password: "" }
 
+  // incorrect login email
+  if(err.message === "incorrect email") {
+    errors.email = "email not registered"
+  }
+
+  // incorrect login password
+  if(err.message === "incorrect password") {
+    errors.password = "incorrect password"
+  }
+
   // duplicate error
   if (err.code === 11000) {
     errors.email = "the email is already in use"
