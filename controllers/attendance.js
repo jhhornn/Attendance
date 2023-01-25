b86921f95ec0ec39c18e5d7ccbcb321089e8cff0
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-25 14:16:49 +0100

    refactor: refactored code to use async and await as well as try/block

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -2,112 +2,118 @@ const User = require("../models/users")
 const fs = require("fs")
 
 const postDetails = async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    image: req.file.filename
-  })
-  user.save((err) => {
-    if (err) {
-      res.json({ message: err.message, type: "danger" })
-    } else {
-      req.session.message = {
-        type: "success",
-        message: "User added successfully"
-      }
-      res.redirect("/")
-    }
-  })
-}
-
-const getHomepage = (req, res) => {
-  User.find().exec((err, users) => {
-    if (err) {
-      res.json({ message: err.message })
-    } else {
-      res.render("pages/index", {
-        title: "Home Page",
-        users: users
-      })
-    }
-  })
-}
-
-const getEditPage = (req, res) => {
-  let id = req.params.id
-  User.findById(id, (err, user) => {
-    if (err) {
-      res.redirect("/")
-    } else {
-      if (user == null) {
-        res.redirect("/")
-      } else {
-        res.render("pages/edit_users", {
-          title: "Edit User",
-          user: user
-        })
-      }
-    }
-  })
-}
-
-const updateDetails = (req, res) => {
-  let id = req.params.id
-  let new_image = ""
-  if (req.file) {
-    new_image = req.file.filename
-    try {
-      fs.unlinkSync("./uploads/" + req.body.old_image)
-    } catch (err) {
-      console.log(err)
-    }
-  } else {
-    new_image = req.body.old_image
-  }
-
-  User.findByIdAndUpdate(
-    id,
-    {
+  try {
+    const user = new User({
       name: req.body.name,
       email: req.body.email,
       phone: req.body.phone,
-      image: new_image
-    },
-    (err, result) => {
-      if (err) {
-        res.json({ message: err.message, type: "danger" })
-      } else {
-        req.session.message = {
-          type: "success",
-          message: "User updated successfully"
-        }
-        res.redirect("/")
-      }
-    }
-  )
+      image: req.file.filename
+    })
+
+   await user.save()
+
+   req.session.message = {
+    type: "success",
+    message: "User added successfully"
+  }
+  res.redirect("/")
+
+  } catch (err) {
+    res.json({ message: err.message, type: "danger" })
+  }
+
 }
 
-const deleteDetails = (req, res) => {
-  let id = req.params.id
-  User.findByIdAndRemove(id, (err, result) => {
+
+const getHomepage = async (req, res) => {
+  try {
+    const users = await User.find({})
+
+    res.render("pages/index", {
+      title: "Home Page",
+      users: users
+    })
+  } catch (err) {
+    res.json({ message: err.message })
+  }
+}
+
+
+const getEditPage = async (req, res) => {
+  try {
+    const id = req.params.id
+
+    const user = await User.findById(id)
+
+    if (user === null) {
+      res.redirect("/")
+    } else {
+      res.render("pages/edit_users", {
+        title: "Edit User",
+        user: user
+      })
+    }
+  } catch (err) {
+    res.redirect("/")
+  }
+}
+
+const updateDetails = async (req, res) => {
+  try {
+    const id = req.params.id
+    let new_image = ""
+    if (req.file) {
+      new_image = req.file.filename
+      try {
+        fs.unlinkSync("./uploads/" + req.body.old_image)
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
+      new_image = req.body.old_image
+    }
+
+    await User.findByIdAndUpdate(
+      id,
+      {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: new_image
+      })
+
+      req.session.message = {
+        type: "success",
+        message: "User updated successfully"
+      }
+      res.redirect("/")
+
+  } catch (err) {
+    res.json({ message: err.message, type: "danger" })
+  }
+}
+
+const deleteDetails = async (req, res) => {
+  try {
+    const id = req.params.id
+    const result = await User.findByIdAndRemove(id)
+
     if (result.image != "") {
       try {
         fs.unlinkSync("./uploads/" + result.image)
-      } catch {
-        console.log(err)
+      } catch (err) {
+        console.log (err)
       }
     }
-    if (err) {
-      res.json({ message: err.message })
-    } else {
-      req.session.message = {
-        type: "info",
-        message: "User deleted successfully"
-      }
-      res.redirect("/")
+
+    req.session.message = {
+      type: "info",
+      message: "User deleted successfully"
     }
-  })
+    res.redirect("/")
+  } catch (err) {
+    res.json({ message: err.message })
+  }
 }
 
 module.exports = {
