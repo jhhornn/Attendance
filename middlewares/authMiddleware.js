a855f2aacec9bf7158cd48e20164cac955ebe525
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-26 19:40:13 +0100

    feat: auth middleware verification

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodedToken) => {
+           if (err) {
+            res.redirect("/login")
+           } else {
+            req.decodedToken = decodedToken
+            next()
+           } 
+        })
+    } else {
+        res.redirect('/login')
+    }
+}
+
+module.exports = requireAuth
